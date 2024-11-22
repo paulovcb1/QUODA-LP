82d06a6a9c5492392c81a4fe49e585855dd76b07
paulo barbosa <paulovcb1@gmail.com>
2024-11-21 21:14:16 -0300

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Header from './components/header';
+import ChatMessages from './components/chatMessages';
+import InputArea from './components/inputArea';
+import { fetchCocktailInfo } from './api/cocktailService';
 
-function App() {
+const App = () => {
+  const [messages, setMessages] = useState([
+    { sender: 'bot', content: 'Olá! Me diga o nome de um coquetel e eu lhe darei informações sobre ele.' },
+  ]);
+
+  const addMessage = (content, sender) => {
+    setMessages((prevMessages) => [...prevMessages, { sender, content }]);
+  };
+
+  const handleSendMessage = (message) => {
+    addMessage(message, 'user');
+    fetchCocktailInfo(message)
+      .then((info) => addMessage(info, 'bot'))
+      .catch(() => addMessage('Desculpe, houve um problema ao se comunicar com o servidor.', 'bot'));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-white w-full max-w-sm h-screen flex flex-col">
+      <Header />
+      <ChatMessages messages={messages} />
+      <InputArea onSendMessage={handleSendMessage} />
     </div>
   );
-}
+};
 
 export default App;
